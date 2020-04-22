3441fafba0751037d32dd563e040a9cccb0ad591
Emma Rose <emrose@mozilla.com>
2020-04-22 19:57:25 -0400

    Update the visual representation of an RP item when new states come in

--- mozilla_session_invalidation/static/main.js
+++ mozilla_session_invalidation/static/main.js
@@ -2,20 +2,50 @@
   const TERMINATE_ENDPT = '/terminate'
   const STATUS_ENDPT = '/status'
   const STATUS_UPDATE_INTERVAL = 500
+  
+  // Constant reliant party identifiers.
+  const RP_SSO = 'sso'
 
   // Handles to document elements that contain inputs and outputs.
   const usernameInput = document.getElementById('username')
   const terminateButton = document.getElementById('terminate')
   const outputsList = document.getElementById('outputs')
-
-
-  // Constant reliant party identifiers.
-  const RP_SSO = 'sso'
+  const reliantParties = {
+    [RP_SSO]: document.getElementById(`rp-${RP_SSO}`),
+  }
 
   // Constant state name representations.
   const STATE_NOT_MODIFIED = 'not_modified'
   const STATE_TERMINATED = 'terminated'
   const STATE_ERROR = 'error'
+
+  // Utility functions.
+  
+  const addOutputItem = function (message, className) {
+    const newItem = document.createElement('li')
+
+    const content = document.createTextNode(message)
+
+    newItem.classList.add(className)
+
+    newItem.appendChild(content)
+
+    outputsList.appendChild(newItem)
+  }
+  
+  const output = function (message) {
+    addOutputItem(message, 'output')
+  }
+
+  const error = function (message) {
+    addOutputItem(message, 'error')
+  }
+
+  const updateStatusView = function (affectedRP, currentState) {
+    console.log(reliantParties)
+    console.log(affectedRP)
+    reliantParties[affectedRP].setAttribute('data-state', currentState)
+  }
 
   // Event handlers.  Each is registered at the end of the function.
  
@@ -34,17 +64,8 @@
       }),
     })
     .then((response) => response.json())
-    .then((data) => {
-      const newItem = document.createElement('li')
-
-      const content = document.createTextNode(JSON.stringify(data))
-
-      newItem.appendChild(content)
-
-      outputsList.appendChild(newItem)
-
-      setInterval(statusUpdate(data['jobId']), STATUS_UPDATE_INTERVAL)
-    })
+    .then((data) =>
+      setInterval(statusUpdate(data['jobId']), STATUS_UPDATE_INTERVAL))
   }
 
   /**
@@ -56,13 +77,16 @@
       fetch(`${STATUS_ENDPT}?jobId=${jobId}`)
       .then((response) => response.json())
       .then((data) => {
-        const newItem = document.createElement('li')
+        for (const result of data.results) {
+          updateStatusView(result['affectedRP'], result['currentState'])
 
-        const content = document.createTextNode(JSON.stringify(data))
-
-        newItem.appendChild(content)
-
-        outputsList.appendChild(newItem)
+          if (result['output'] !== null) {
+            output(result['output'])
+          }
+          if (result['error'] !== null) {
+            error(result['error'])
+          }
+        }
       })
     }
   }
