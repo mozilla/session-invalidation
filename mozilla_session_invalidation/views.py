002c5bf8e6f278998c1c6d8f5beb93cab2749eda
Emma Rose <emrose@mozilla.com>
2020-04-22 18:57:08 -0400

    Starting an endpoint for fetching status updates

--- mozilla_session_invalidation/views.py
+++ mozilla_session_invalidation/views.py
@@ -1,9 +1,12 @@
 import typing as types
 
-from flask import render_template, request
+from flask import render_template, request, session
 
 from mozilla_session_invalidation import app, socketio
 import mozilla_session_invalidation.messages as msgs
+from mozilla_session_invalidation.session_invalidation import\
+    SupportedReliantParties,\
+    TerminationState
 
 
 def generate_id(is_unique: types.Callable[[str], bool]) -> str:
@@ -22,9 +25,25 @@ def index():
 @app.route('/terminate', methods=['POST'])
 def terminate():
     # TODO:  Store a list of all job IDs in the app global context.
-    new_id = generate_id(lambda _: True)
+    session.job_id = generate_id(lambda _: True)
 
     # TODO:  Retrieve OAuth credentials from request cookies.
     oauth_tkn = ''
 
     return msgs.Result([]).to_json()
+
+
+@app.route('/status')
+def status():
+    job_id = request.args.job_id
+
+    if session.job_id != job_id:
+        return msgs.Error('Invalid job ID').to_json()
+
+    return msgs.Result([
+        msgs.Status(
+            affected_rp=SupportedReliantParties.SSO,
+            current_state=TerminationState.TERMINATED,
+            output=None,
+            error=None)
+    ])
