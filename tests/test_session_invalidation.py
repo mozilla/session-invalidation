46b1b02ed611a06424d3816b340a8aa1c5bd04ee
Emma Rose <emrose@mozilla.com>
2020-05-01 17:30:59 -0400

    Wrote tests for terminate_gsuite

--- tests/test_session_invalidation.py
+++ tests/test_session_invalidation.py
@@ -46,3 +46,52 @@ class TestSessionInvalidators(unittest.TestCase):
 
             assert result.error is None
             assert result.new_state == sesinv.TerminationState.TERMINATED
+
+    def test_terminate_gsuite_error_handling(self):
+        with requests_mock.Mocker() as mock:
+            mock.patch(requests_mock.ANY, status_code=400)
+
+            terminate = sesinv.terminate_gsuite(
+                'testtoken',
+                'http://test.com/endpoint/{}',
+            )
+
+            result = terminate('testuser@mozilla.com')
+
+            history = mock.request_history
+            assert len(history) == 2
+
+            e_url = 'test.com/endpoint/testuser@mozilla.com'
+            assert history[0].url.endswith(e_url)
+            assert history[1].url.endswith(e_url)
+
+            assert history[0].json()['changePasswordAtNextLogin'] is True
+            assert history[1].json()['changePasswordAtNextLogin'] is False
+
+            assert result.error is not None
+            assert 'Status 400' in result.error
+            assert result.new_state == sesinv.TerminationState.ERROR
+
+    def test_terminate_gsuite_success_case(self):
+        with requests_mock.Mocker() as mock:
+            mock.patch(requests_mock.ANY, status_code=200)
+
+            terminate = sesinv.terminate_gsuite(
+                'testtoken',
+                'http://test.com/endpoint/{}',
+            )
+
+            result = terminate('testuser@mozilla.com')
+
+            history = mock.request_history
+            assert len(history) == 2
+
+            e_url = 'test.com/endpoint/testuser@mozilla.com'
+            assert history[0].url.endswith(e_url)
+            assert history[1].url.endswith(e_url)
+
+            assert history[0].json()['changePasswordAtNextLogin'] is True
+            assert history[1].json()['changePasswordAtNextLogin'] is False
+
+            assert result.error is None
+            assert result.new_state == sesinv.TerminationState.TERMINATED
