791d8ae385d5b1d8a52c43f6f2a694da6112669a
Emma Rose <emrose@mozilla.com>
2020-05-01 16:50:12 -0400

    Fix test code

--- tests/test_session_invalidation.py
+++ tests/test_session_invalidation.py
@@ -2,13 +2,13 @@ import unittest
 
 import requests_mock
 
-import mozdef_session_invalidation.session_invalidation as sesinv
+import mozilla_session_invalidation.session_invalidation as sesinv
 
 
 class TestSessionInvalidators(unittest.TestCase):
     def test_terminate_sso_error_handling(self):
         with requests_mock.Mocker() as mock:
-            mock.post('www.test.com/endpoint', status_code=400)
+            mock.post(requests_mock.ANY, status_code=400)
 
             terminate = sesinv.terminate_sso(
                 'testtoken',
@@ -20,15 +20,16 @@ class TestSessionInvalidators(unittest.TestCase):
             history = mock.request_history
             assert len(history) == 1
 
-            e_url = 'http://www.test.com/endpoint/ad%7CMozilla-LDAP%7Ctestuser'
-            assert history[0].url == e_url
+            e_url = 'test.com/endpoint/ad%7CMozilla-LDAP%7Ctestuser'
+            assert history[0].url.endswith(e_url)
 
             assert result.error is not None
+            assert 'Status 400' in result.error
             assert result.new_state == sesinv.TerminationState.ERROR
     
     def test_terminate_sso_success_case(self):
         with requests_mock.Mocker() as mock:
-            mock.post('www.test.com/endpoint', status_code=204)
+            mock.post(requests_mock.ANY, status_code=204)
 
             terminate = sesinv.terminate_sso(
                 'testtoken',
@@ -40,8 +41,8 @@ class TestSessionInvalidators(unittest.TestCase):
             history = mock.request_history
             assert len(history) == 1
 
-            e_url = 'http://www.test.com/endpoint/ad%7CMozilla-LDAP%7Ctestuser'
-            assert history[0].url == e_url
+            e_url = 'test.com/endpoint/ad%7CMozilla-LDAP%7Ctestuser'
+            assert history[0].url.endswith(e_url)
 
             assert result.error is None
             assert result.new_state == sesinv.TerminationState.TERMINATED
