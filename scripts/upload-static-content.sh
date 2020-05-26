3a7ec90c734bea192cb825ac306c5468f0e1a8ad
Arcadia Rose <arose@mozilla.com>
2020-05-26 13:05:30 -0400

    Script to upload static content

--- scripts/upload-static-content.sh
+++ scripts/upload-static-content.sh
@@ -1,6 +1,5 @@
 BUCKET_NAME="session-invalidation-static-content"
 
 for filename in $(ls static); do
-  echo "Uploading $filename";
   aws s3 cp "static/$filename" "s3://$BUCKET_NAME/$filename";
 done
