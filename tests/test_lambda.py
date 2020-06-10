07e3db2267caac4f4181484c2d2dd31271ff18e5
Arcadia Rose <arose@mozilla.com>
2020-06-10 16:05:02 -0400

    Patch load_config so that it doesn't reach out to SSM

--- tests/test_lambda.py
+++ tests/test_lambda.py
@@ -3,7 +3,10 @@ import os
 import unittest
 from unittest.mock import patch
 
+import ecdsa
 import requests_mock
+
+import sesinv
 
 # Since the entrypoints for our lambda functions are in a file named after a
 # Python keyword, we import it as follows and give it a non-keyword name.
@@ -33,6 +36,8 @@ def load_test_env_vars(**kwargs):
 
     os.environ.update(defaults)
 
+    return defaults
+
 
 @patch('lambda.log', lambda _, __: None)
 class TestOIDCClientFlow(unittest.TestCase):
@@ -43,10 +48,15 @@ class TestOIDCClientFlow(unittest.TestCase):
     is simulated by manually constructing events.
     '''
 
-    def test_redirect_to_op_authorize_endpoint(self):
+    @patch('lambda.load_config')
+    def test_redirect_to_op_authorize_endpoint(self, load_config_mock):
+        load_config_mock.return_value = load_test_env_vars(
+            OIDC_DISCOVERY_URI=\
+                'http://test.site.com/.well-known/oidc-configuration',
+        )
+
         event = {
             'headers': {},
-            'body': '',
         }
 
         discovery_doc = {
@@ -65,11 +75,6 @@ class TestOIDCClientFlow(unittest.TestCase):
             ]
         }
 
-        load_test_env_vars(
-            OIDC_DISCOVERY_URI=\
-                'http://test.site.com/.well-known/oidc-configuration',
-        )
-
         with requests_mock.Mocker() as mock:
             mock.get(os.environ['OIDC_DISCOVERY_URI'], json=discovery_doc)
             mock.get(discovery_doc['jwks_uri'], json=jwks_doc)
@@ -80,3 +85,24 @@ class TestOIDCClientFlow(unittest.TestCase):
 
         assert response['statusCode'] == 302
         assert location == discovery_doc['authorization_endpoint']
+
+    @patch('lambda.load_config')
+    def test_no_redirect_for_authenticated_user(self, load_config_mock):
+        load_config_mock.return_value = load_test_env_vars(
+            SIGNING_KEY_ECDSA=ecdsa.SigningKey.generate().to_string().hex(),
+        )
+
+        session_token = sesinv.authentication.generate_auth_cookie(
+            os.environ['SIGNING_KEY_ECDSA'],
+        )
+
+        event = {
+            'headers': {
+                'Cookie': f'{main.USER_SESSION_COOKIE_KEY}={session_token}',
+            },
+        }
+
+        response = main.index(event, None)
+
+        assert response['statusCode'] == 200
+        assert response['headers']['Content-Type'] == 'text/html'
